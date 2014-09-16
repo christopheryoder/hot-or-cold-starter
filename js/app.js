5a6cdd5a4ea44273322696a66ba5522d7fd65b23
christopheryoder <christophercyoder@gmail.com>
2014-09-16 14:20:30 -0400

    Finished all functionality for Hot and Cold app.

--- js/app.js
+++ js/app.js
@@ -1,96 +1,135 @@
+$(document).ready(function () {
+	$("#userGuess").focus();
+/*--- Display information modal box ---*/
+	$(".what").click(function () {
+		$(".overlay").fadeIn(1000);
+	});
 
-$(document).ready(function(){
-	
-	/*--- Display information modal box ---*/
-  	$(".what").click(function(){
-    	$(".overlay").fadeIn(1000);
-  	});
+/*--- Hide information modal box ---*/
+	$("a.close").click(function () {
+ 		$(".overlay").fadeOut(1000);
+    });
 
-  	/*--- Hide information modal box ---*/
-  	$("a.close").click(function(){
-  		$(".overlay").fadeOut(1000);
-  	});
+/*Declare global variables*/
+	var secretNumber;
+	var userGuess;
+	var guessCount = 0;
 
-  	/*Declare global variables*/
-  	var secretNumber = Math.floor(Math.random()*100);
-  	var userGuess;
-  	var guessCount = 0;
+/*Declare global functions*/
 
-  	/*Declare global functions*/
-
-	var postFeedback = function(message) {
-	$('#feedback').text(message);
+	var postFeedback = function (message) {
+		$('#feedback').text(message);
 	};
 
-  	var setSecretNumber = function() {
-  		secretNumber = Math.floor(Math.random()*100);
-  	};
+	var setSecretNumber = function () {
+		secretNumber = Math.floor(Math.random() * 100);
+		console.log("Secret Number: " + secretNumber);
+	};
 
-  	var setUserGuess = function() {
-  		userGuess = $("#userGuess").val();
-  	};
+	var setUserGuess = function () {
+		userGuess = Number($("#userGuess").val());
+	};
 
-  	var postGuessCount = function() {
-  		$("#count").text(guessCount);
-  	};
+	var postGuessCount = function () {
+		$("#count").text(guessCount);
+	};
 
-  	var clearGuessBox = function() {
-  		$("#userGuess").val("");
-  	};
+	var clearGuessBox = function () {
+		$("#userGuess").val("");
+	};
 
-  	var newGame = function() {
-		setSecretNumber();
-		postFeedback("Make your Guess!");
+	var clearGuessList = function () {
+		$("#guessList").children().remove();
+	};
+
+	var newGame = function () {
+	setSecretNumber();
+	postFeedback("Make your Guess!");
+	clearGuessBox();
+	clearGuessList();
+	guessCount = 0;
+	postGuessCount();
+	console.log("newGame ran");
+	/*more stuff*/
+	};
+
+	newGame();
+
+	var addGuessToList = function () {
+	$("#guessList").append("<li>"+userGuess+"</li>");
+	};
+
+/*compares user guess and secret number and posts feedback*/
+	var guessTheNumber = function () {
+		
+		setUserGuess();
+		
 		clearGuessBox();
-		guessCount = 0;
-		postGuessCount();
-		/*more stuff*/
-  	};
 
-  	/*compares user guess and secret number and posts feedback*/
-  	var compare = function() {
-		var difference = Math.abs(userGuess-secretNumber);
-		if (userGuess==secretNumber) {
+		var updateGuessInfo = function () {
+			guessCount++;
+			postGuessCount();
+			addGuessToList();
+		};
+
+		var difference = Math.abs(userGuess - secretNumber);
+
+		if (secretNumber === "guessed") {
+			postFeedback("You Won this game already! You need to start a new game.");
+		}
+		else if (userGuess > 100 || userGuess < 1) {
+			postFeedback("Oops! Your guess has to be a number between 1 and 100!");
+		}
+
+		else if (userGuess === secretNumber) {
 			postFeedback("You are correct!");
-			/*Give user the option to start a new game*/
-			var okCancel = confirm("You won the game! : )\nWould you like to play again?");
-			if (okCancel == true) {
-				newGame();
-				return;
-			} 
-			else {
+			updateGuessInfo();
+			secretNumber = "guessed";
+		}
 
-			}
-		}
-		else if(difference >= 1 && difference <= 3) {
+		else if(difference === 1) {
 			postFeedback("You are burning up!");
+			updateGuessInfo();
 		}
-		else if(difference >= 4 && difference <= 6) {
+
+		else if(difference >= 2 && difference <= 3) {
 			postFeedback("You are very hot!");
+			updateGuessInfo();
 		}
-		else if(difference >= 7 && difference <= 13) {
+
+		else if(difference >= 4 && difference <= 7) {
 			postFeedback("You are hot");
+			updateGuessInfo();
 		}
-		else if(difference >= 14 && difference <= 25) {
+
+		else if(difference >= 8 && difference <= 15) {
 			postFeedback("You are warm");
+			updateGuessInfo();
 		}
-		else if (difference >= 26 && difference <= 50) {
-			postFeedback("You are cold");
+
+		else if(difference >= 16 && difference <= 31) {
+			postFeedback("You are lukewarm");
+			updateGuessInfo();
 		}
+
+		else if (difference >= 32 && difference <= 63) {
+			postFeedback("You are cool");
+			updateGuessInfo();
+		}
+
 		else {
 			postFeedback("You are ice cold!");
+			updateGuessInfo();
 		}
-		guessCount++;
-		postGuessCount();
+		$("#userGuess").focus();
 	};
 
-	/*What happens when the user clicks the guess button*/
+/*What happens when the user clicks the guess button*/
+/*Try using an on submit event rather than an on click*/
+/*First thing you want to do is validate*/
 	$('#guessButton').click(function(event) {
+		guessTheNumber();
 		event.preventDefault();
-		setUserGuess();
-		clearGuessBox();
-		compare();
-		$("#guessList").append("<li>"+userGuess+"</li>");
 	});
 
 	/*What happens when the user clicks on the New Game button*/
